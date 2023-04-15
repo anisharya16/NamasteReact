c159ecc1c4254899f9cd97fd3bfa27ad45b65abf
Anish Kumar Arya <anisharya16@gmail.com>
2023-04-15 15:07:08 +0530

    Chapter 5

---
+++ Chapter 5/src/components/Body.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { restaurantList } from "../constants";
+import RestaurantCard from "./RestaurantCard";
+
+function filterData(searchText, restaurants) {
+  const searchedRestaurant = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  return searchedRestaurant;
+}
+
+const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const searchFilteredData = filterData(searchText, restaurantList);
+            setRestaurants(searchFilteredData);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((fetchedData) => {
+          return (
+            <RestaurantCard {...fetchedData.data} key={fetchedData.data.id} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
