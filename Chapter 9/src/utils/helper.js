6b258128b0d3469d39dcc1a50b18edaefcaa0acb
Anish Kumar Arya <anisharya16@gmail.com>
2023-10-25 21:01:03 +0530

    api change

--- Chapter 9/src/utils/helper.js
+++ Chapter 9/src/utils/helper.js
@@ -1,6 +1,6 @@
 export function filterData(searchText, restaurants) {
   const searchedRestaurant = restaurants.filter((restaurant) =>
-    restaurant?.data?.name?.toLowerCase().includes(searchText.toLowerCase())
+    restaurant?.info?.name?.toLowerCase().includes(searchText.toLowerCase())
   );
   return searchedRestaurant;
 }
