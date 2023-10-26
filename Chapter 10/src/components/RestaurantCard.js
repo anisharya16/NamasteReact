83b992a8dbe8fbbef8509b54ec523cbc4cc4a6eb
Anish Kumar Arya <anisharya16@gmail.com>
2023-10-26 12:54:16 +0530

    minor changes

--- Chapter 10/src/components/RestaurantCard.js
+++ Chapter 10/src/components/RestaurantCard.js
@@ -4,9 +4,9 @@ const RestaurantCard = ({ name, cloudinaryImageId, cuisines, area }) => {
   return (
     <div className="w-60 p-2 hover:bg-red-400 shadow-xl rounded-md sm:bg-purple-500 md:bg-red-500 lg:bg-blue-300">
       <img src={IMG_CDN_URL + cloudinaryImageId} alt="food_image" />
-      <h2 className="font-bold text-gray-700 text-xl underline my-2">{name}</h2>
-      <h3>{cuisines.join(", ")}</h3>
-      <h4>{area}</h4>
+      <p className="font-bold text-gray-700 text-xl underline my-2">{name}</p>
+      <p>{cuisines.join(", ")}</p>
+      <p>{area}</p>
     </div>
   );
 };
