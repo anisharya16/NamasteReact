7ec0099b66abebf1b2f67d14d1e45bc7ad2a0aba
Anish Kumar Arya <anisharya16@gmail.com>
2023-10-29 16:01:35 +0530

    redux architecture workflow comment

--- Chapter 12/src/utils/store.js
+++ Chapter 12/src/utils/store.js
@@ -8,3 +8,9 @@ const store = configureStore({
 });
 
 export default store;
+
+// Redux Architecture (Workflow)
+// When we click on Add button, an action is dispatched which will call a reducer
+// function which will update the slice in the store.
+// Using selector we will get the data by subscribing to the store accessing a particular portion of our slice
+// and getting the data onto our app.
