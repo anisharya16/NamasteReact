f79dff2990402bf5e4aa3640c3e4703d49e4ba72
Anish Kumar Arya <anisharya16@gmail.com>
2024-09-22 17:28:18 +0530

    Revert "maching coding round - building youtube"
    This reverts commit 52cfd26f480f832fb597a6f5fe32b6440447b4c5.
    
    Reverting last change

--- Chapter 14/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-const GOOGLE_API_KEY = "AIzaSyClChjjw-P2-jgSBWV7PRPRFSFzP8zJ9gc";
-export const YOUTUBE_API_URL =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  GOOGLE_API_KEY;
