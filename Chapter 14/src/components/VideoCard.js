52cfd26f480f832fb597a6f5fe32b6440447b4c5
Anish Kumar Arya <anisharya16@gmail.com>
2024-09-22 17:15:04 +0530

    maching coding round - building youtube

---
+++ Chapter 14/src/components/VideoCard.js
@@ -0,0 +1,21 @@
+const VideoCard = ({ info }) => {
+  const { snippet, statistics } = info;
+  const { channelTitle, title, thumbnails } = snippet;
+
+  return (
+    <div className="p-2 w-80 shadow-lg h-full">
+      <img
+        className="rounded-lg "
+        src={thumbnails?.maxres?.url}
+        alt="thumbnail"
+      />
+      <ul>
+        <li className="font-bold py-2">{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics?.viewCount} views</li>
+      </ul>
+    </div>
+  );
+};
+
+export default VideoCard;
