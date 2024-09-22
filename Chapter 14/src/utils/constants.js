2c76a48ab82af8e2752e99cc406da4c8acac13d8
Anish Kumar Arya <anisharya16@gmail.com>
2024-09-22 17:32:22 +0530

    maching coding round

---
+++ Chapter 14/src/utils/constants.js
@@ -0,0 +1,4 @@
+const GOOGLE_API_KEY = "use-your-youtube-api-key";
+export const YOUTUBE_API_URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
